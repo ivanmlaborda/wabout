e7eac7e8ea6ef65b86017ea6f1e40ed22efbd9b8
ivanmlaborda <ivanmlaborda@gmail.com>
2017-08-22 09:31:10 +0200

    fonts ok

--- client/js/routes/welcome/controllers/mapCtrl.js
+++ client/js/routes/welcome/controllers/mapCtrl.js
@@ -8,7 +8,7 @@
     $rootScope.logged = false
 
     let map = L.map('map', {
-      center: [41.610, 0.630],
+      center: [41.603, 0.626],
       zoom: 15
     })
     L.tileLayer('http://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}.png', {
@@ -16,6 +16,8 @@
       subdomains: 'abcd',
       maxZoom: 19
     }).addTo(map)
+
+    let samplePoint = L.marker([41.598, 0.629]).addTo(map);
 
     // L.tileLayer('http://{s}.tile.openstreetmap.se/hydda/full/{z}/{x}/{y}.png', {
     //   attribution: 'Tiles courtesy of <a href="http://openstreetmap.se/" target="_blank">OpenStreetMap Sweden</a> &mdash; Map data &copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
