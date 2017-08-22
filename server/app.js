c045b23176a3ea0860d7e1886cec03d19d8d39cb
ivanmlaborda <ivanmlaborda@gmail.com>
2017-08-22 16:44:35 +0200

    process.env.PORT

--- server/app.js
+++ server/app.js
@@ -4,7 +4,7 @@ const path = require('path')
 const app = express()
 const pathPublic = path.join(process.cwd(), 'client')
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 
 // const routerMap = require('./routes/map')
 
