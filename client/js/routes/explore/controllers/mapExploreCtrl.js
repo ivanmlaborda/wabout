ba098226480b32e89d7adb87cc6559a288cf67ad
ivanmlaborda <ivanmlaborda@gmail.com>
2017-08-23 20:22:25 +0200

    leaflet angular version

--- client/js/routes/explore/controllers/mapExploreCtrl.js
+++ client/js/routes/explore/controllers/mapExploreCtrl.js
@@ -6,13 +6,34 @@
     // OJO SOLO PARA DESARROLLO FRONT
     $rootScope.logged = true
     console.log('mapExploreCtrl Loaded')
-
-    // Default view
-    $scope.userView = {
-      'lat': 0,
-      'lng': 0,
-      'zoom': 18
+    let mainMarker = {
+      lat: 0,
+      lng: 0,
+      focus: true,
+      message: "You're here"
+      // draggable: true
     }
+
+    angular.extend($scope, {
+      userView: {
+        lat: 51.505,
+        lng: -0.09,
+        zoom: 8
+      },
+      markers: {
+        mainMarker: angular.copy(mainMarker)
+      },
+      defaults: {
+        tileLayer: 'http://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}.png',
+        zoomControlPosition: 'topright',
+        tileLayerOptions: {
+          opacity: 0.9,
+          detectRetina: true,
+          reuseTiles: true
+        }
+        // ,scrollWheelZoom: false
+      }
+    })
 
     GeolocateService.getGeolocation()
       .then(userCoords => {
