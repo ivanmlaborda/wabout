88385307ab46c5a210ff9c69973397c005979873
ivanmlaborda <ivanmlaborda@gmail.com>
2017-08-24 11:51:18 +0200

    movement map /icon

--- client/js/routes/explore/controllers/mapExploreCtrl.js
+++ client/js/routes/explore/controllers/mapExploreCtrl.js
@@ -23,7 +23,7 @@
         },
         // ,scrollWheelZoom: false
         defaultIcon: {
-          iconUrl: 'img/marker-icon.png',
+          iconUrl: '/img/marker-icon.png',
           iconSize:     [25, 41], // size of the icon
           iconAnchor:   [22, 41], // point of the icon which will correspond to marker's location
           popupAnchor:  [-3, -76] // point from which the popup should open relative to the iconAnchor
@@ -39,7 +39,7 @@
             lng: lng,
             focus: true,
             message: "You're here!",
-            icon: 'defaultIcon'
+            // icon: 'defaultIcon'
           }
         }
       })
@@ -57,6 +57,17 @@
         })
         $scope.addMarkers($scope.userCoords.lat, $scope.userCoords.lng)
       })
+
+
+        // TESTING REFRESH SCOPE
+        setInterval(() => {
+          $scope.$apply(() => {
+          $scope.markers.meMarker.lat = $scope.markers.meMarker.lat + 0.0005
+          console.log($scope.markers.meMarker.lat)      })
+        }, 3000)
+
+
+
   }
 
   angular
