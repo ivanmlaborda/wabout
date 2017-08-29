94b370d26058c2ea00666d706b4da94290d8fbcc
ivanmlaborda <ivanmlaborda@gmail.com>
2017-08-29 21:26:21 +0200

    settings ngrepeats

--- client/js/routes/settings/controllers/settingsCtrl.js
+++ client/js/routes/settings/controllers/settingsCtrl.js
@@ -1,12 +1,16 @@
 /* global angular */
 (function () {
   'use strict'
-  function settingsCtrl ($rootScope) {
-    console.log('settingsCtrl Loaded')
-
-    //OJO SOLO PARA DESARROLLO FRONT!
+  function settingsCtrl ($scope, $rootScope, DataService) {
+    // OJO SOLO PARA DESARROLLO FRONT!
     $rootScope.logged = true
+    $rootScope.userName = 'ivan'
+    console.log('settingsCtrl Loaded')
+    $scope.contacts = []
 
+    DataService.getUserIdByUserName($rootScope.userName)
+      .then(data => DataService.getContactsByUserId(data.data._id))
+      .then(data => data.data.contacts.forEach(key => $scope.contacts.push(key.userId)))
   }
   angular
     .module('Wabout')
