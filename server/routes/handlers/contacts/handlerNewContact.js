c2f786d18dc8e7743ed3092c5b9248fb96e00fb9
ivanmlaborda <ivanmlaborda@gmail.com>
2017-08-29 10:13:48 +0200

    promise ok newcontact

--- server/routes/handlers/contacts/handlerNewContact.js
+++ server/routes/handlers/contacts/handlerNewContact.js
@@ -4,31 +4,19 @@ const mongoose = require('mongoose')
 const ObjectId = mongoose.Schema.Types.ObjectId
 
 function newContact (req, res) {
-
   const { userName } = req.params
   const { contactName } = req.body
-  console.log(userName)
-  console.log(contactName)
-  let contactId = ''
+
   User
-    .find({userName: contactName}, {_id:1})
-    .then(function(id){
-      id.map(function (id) {
-        contactId = id._id
-      })
-      console.log(contactId)
-    })
-    .then(
-      User.update(
-        { userName: userName },
-        {$push: {contacts: {id: ObjectId(contactId)}}}
+    .find({userName: contactName}, {_id: 1})
+    .then(user => user._id)
+    .then(userId => User.update({ userName },
+        {$push: {contacts: {id: ObjectId(userId)}}}
       )
-      .then(data => {
-        console.log(data)
-        res.send(`${contactName} with id ${contactId} added tou your contact list`)
-      })
-    // .then(console.log(contactId))
     )
+    .then(userUpdated => {
+      res.send(`${contactName} added tou your contact list`)
+    })
 }
 
 module.exports = newContact
