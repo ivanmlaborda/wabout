7807f6ab01b40acda953a687b3fbf870d1b6075a
ivanmlaborda <ivanmlaborda@gmail.com>
2017-08-31 19:50:54 +0200

    unique contacts OK

--- server/routes/handlers/contact/handlerNewContact.js
+++ server/routes/handlers/contact/handlerNewContact.js
@@ -1,18 +1,34 @@
 const User = require('../../../models/User')
 
-const mongoose = require('mongoose')
-const ObjectId = mongoose.Schema.Types.ObjectId
-
 function newContact (req, res) {
   const { userName } = req.params
   const { contactName } = req.body
 
   User
-    .findOne({userName: contactName}, {_id: 1})
-    .then(user => user._id)
-    .then(userId => User.update({ userName }, {$push: {contacts: {userId}}}))
-    .then(userUpdated => {
-      res.send('contact added')
+    .findOne({userName})
+    .populate('contacts.userId')
+    .then(user => {
+      let alreadyExists = false
+      const hasContact = user.contacts.forEach(contact => {
+        if (contact.userId.userName === contactName) {
+          alreadyExists = true
+        }
+      })
+
+      if (alreadyExists) {
+        res.send({ result: 'KO', message: 'Your contact is already in your contact list' })
+      } else {
+        User
+          .findOne({userName: contactName}, {_id: 1})
+          .then(user => user._id)
+          .then(userId => User.update({ userName }, {$push: {contacts: {userId}}}))
+          .then(userUpdated => {
+            res.send({ result: 'OK', message: 'contact added to user successfully' })
+          })
+          .catch(error => {
+            res.send({ result: 'KO', message: 'an error happened on adding a contact to user', error })
+          })
+      }
     })
 }
 
