ab87fe38cd4de56cf8c39c1842e939f82f59b527
ivanmlaborda <ivanmlaborda@gmail.com>
2017-09-02 13:54:30 +0200

    checkboxes privacy ok

--- client/js/app.js
+++ client/js/app.js
@@ -1,6 +1,6 @@
 /* global angular */
 (function () {
-  angular.module('Wabout', ['ngRoute', 'leaflet-directive'])
+  angular.module('Wabout', ['ngRoute', 'leaflet-directive', 'checklist-model'])
   .run(function () {
     console.log('Angular loaded')
   })
--- client/js/routes/settings/controllers/settingsCtrl.js
+++ client/js/routes/settings/controllers/settingsCtrl.js
@@ -7,10 +7,26 @@
     // $rootScope.userName = 'ivan'
     console.log('settingsCtrl Loaded')
     $scope.contacts = []
+    $scope.granteds = {
+      contacts : []
+    }
 
     DataService.getUserIdByUserName($rootScope.userName)
       .then(data => DataService.getContactsByUserId(data.data._id))
-      .then(data => data.data.contacts.forEach(key => $scope.contacts.push(key.userId)))
+      .then(data => data.data.contacts.forEach(contact => $scope.contacts.push(contact)))
+      .then(() => $scope.contacts.forEach(contact => {
+        if (contact.shareTo) {
+          $scope.granteds.contacts.push(contact._id)
+        }
+      }))
+      .then(console.log($scope.contacts))
+      .then(console.log($scope.granteds.contacts))
+
+
+    $scope.updatePrivacy = () => {
+      console.log('submit')
+      console.log($scope.granteds.contacts)
+    }
   }
   angular
     .module('Wabout')
