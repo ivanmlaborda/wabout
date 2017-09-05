b0796c18542034759878de7e16b81c11d6039a86
ivanmlaborda <ivanmlaborda@gmail.com>
2017-09-05 17:41:44 +0200

    register redirection

--- client/js/routes/auth/register/controllers/formRegisterCtrl.js
+++ client/js/routes/auth/register/controllers/formRegisterCtrl.js
@@ -1,12 +1,15 @@
 /* global angular */
 (function () {
   'use strict'
-  function formRegisterCtrl (AuthService, toastr) {
+  function formRegisterCtrl (AuthService, toastr, $location) {
     this.register = (e) => {
       e.preventDefault()
       AuthService.register(this.username, this.password)
         .then(data => {
-          if (data.success) toastr.success(data.msg)
+          if (data.success) {
+            toastr.success(data.msg)
+            $location.path('/login')
+          }
           else toastr.error(data.msg)
         })
     }
